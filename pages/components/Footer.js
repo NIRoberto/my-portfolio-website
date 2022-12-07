7c3810ff2a0d24e8b5c07036150c650f9c7e3e6e
niroberto <robertwilly668@gmail.com>
2022-12-07 12:23:49 +0200

    add  footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -3,38 +3,44 @@ import { FaFacebookSquare, FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-orange-400 p-16 w-full border-2 border-white text-white flex justify-between items-center">
+    <footer className="bg-orange-400 p-16 w-full text-white flex justify-between items-center">
       <div className="basis-3/6">
-        <h1 className="text-2xl">Get in Touch</h1>
-        <form action="#" className="p-  border-2  border-red-50">
-          <div className="flex">
+        <h1 className="text-4xl font-semibold py-2  px-10">Get in Touch</h1>
+        <form action="#" className=" p-4">
+          <div className="flex px-4 justify-around w-full">
             <div className="flex flex-col">
-              <label htmlFor="Name">Name</label>
+              <label className="py-2" htmlFor="Name">
+                Name
+              </label>
               <input
                 type="text"
                 name="name"
-                className=" py-2  outline-none  border-none px-8 rounded-md"
+                className=" py-2 w-full outline-none  border-none px-8 rounded-md"
               />
             </div>
-            <div className="flex  outline-none  border-none flex-col">
-              <label htmlFor="Email">Email</label>
+            <div className="flex   outline-none  border-none flex-col">
+              <label className="py-2" htmlFor="Email">
+                Email
+              </label>
               <input
                 type="text"
                 name="email"
-                className=" py-2  outline-none  border-none px-8 rounded-md"
+                className=" py-2 text-gray-500 outline-none  border-none px-8 rounded-md"
               />
             </div>
           </div>
-          <div className="flex flex-col  outline-none  border-none  rounded-md  py-2 px-8">
-            <label htmlFor="message">Message</label>
+          <div className="flex flex-col  outline-none   border-none  rounded-md  py-2 px-8">
+            <label className="py-2" htmlFor="message">
+              Message
+            </label>
             <textarea
-              className="h-20 outline-none border-none  rounded-md"
+              className="h-32 p-2 text-black outline-none border-none  rounded-md"
               name="message"
               id="message"
             ></textarea>
           </div>
-          <div>
-            <button className="rounded-md py-20  outline-none  border-none px-8">
+          <div className="px-8">
+            <button className="rounded-md mt-4 w-full py-2 bg-white text-center text-orange-400 outline-none  border-none px-8">
               Send message
             </button>
           </div>
@@ -42,20 +48,20 @@ const Footer = () => {
       </div>
       <div className="basis-3/6">
         <div className="flex flex-col">
-          <span>Phone</span>
+          <span className="font-semibold py-2 text-2xl">Phone</span>
           <span>+250787491277</span>
         </div>
         <div className="flex flex-col">
-          <span>Email</span>
+          <span className="font-semibold py-2 text-2xl">Email</span>
           <span>robertwilly668@gmail.com</span>
         </div>
         <div className="flex flex-col">
-          <span>Address: </span>
+          <span className="font-semibold py-2 text-2xl">Address: </span>
           <span>Huye,Southern Rwanda</span>
         </div>
-        <div className="flex">
+        <div className="flex ">
           <span>
-            <FaFacebookSquare />
+            <FaFacebookSquare className="" />
           </span>
           <span>
             <FaGithubSquare />
